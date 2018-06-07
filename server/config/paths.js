024460404183240426c327f79313acdf522c0dbb
slyngstad60 <kevan.slyngstad@geckodesigns.com>
2018-06-07 11:43:22 -0600

    reconfigured app to hot reload with dev server

--- server/config/paths.js
+++ server/config/paths.js
@@ -9,8 +9,8 @@ const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 export default {
 	appClient: resolveApp('../client'),
 	appIndexJs: resolveApp('../client/src/index.js'),
-	appOutput: resolveApp('../client/src/public'),
-	appHtml: resolveApp('../client/src/public/index.html'),
+	appOutput: resolveApp('../client/public'),
+	appHtml: resolveApp('../client/public/index.html'),
 	// serverStart: resolveApp('src/app.js'),
 	appSrc: resolveApp('../client/src'),
 };
